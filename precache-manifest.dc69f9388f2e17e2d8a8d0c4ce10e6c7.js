51a541caf1fddfbe0c5a40592b0877924d8cf87d
Mateus <mateusandreferreira@gmail.com>
2021-03-15 04:21:05 -0400

    gh-pages

---
+++ precache-manifest.dc69f9388f2e17e2d8a8d0c4ce10e6c7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50d864269f54eccb147",
+    "url": "/prokemon-cards/css/app.728690b6.css"
+  },
+  {
+    "revision": "c9e2d954fd6ac4f42200",
+    "url": "/prokemon-cards/css/chunk-70ba2eea.4ef5c798.css"
+  },
+  {
+    "revision": "83d6b10f76a5d00417888afdf053ad1a",
+    "url": "/prokemon-cards/img/pokeball-color.83d6b10f.png"
+  },
+  {
+    "revision": "13a3874bc6761af374ef618c3da56665",
+    "url": "/prokemon-cards/img/pokeball-loading-unscreen.13a3874b.gif"
+  },
+  {
+    "revision": "3e2a714efa6a3ecfa826556a5780b508",
+    "url": "/prokemon-cards/index.html"
+  },
+  {
+    "revision": "c50d864269f54eccb147",
+    "url": "/prokemon-cards/js/app.a12c65b4.js"
+  },
+  {
+    "revision": "c9e2d954fd6ac4f42200",
+    "url": "/prokemon-cards/js/chunk-70ba2eea.64813972.js"
+  },
+  {
+    "revision": "afb251e503af1996e85e",
+    "url": "/prokemon-cards/js/chunk-vendors.9cdcc4a4.js"
+  },
+  {
+    "revision": "f80aa54e78f62a18aa413917e23c5b84",
+    "url": "/prokemon-cards/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/prokemon-cards/robots.txt"
+  }
+]);
