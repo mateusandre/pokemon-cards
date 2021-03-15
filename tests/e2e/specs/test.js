39caf431789596ea21421e0bcd1bb00cc011a89d
Mateus <mateusandreferreira@gmail.com>
2021-03-15 19:19:40 -0400

    Adiciona testes e2e para abrir a home page, pesquisar um pokemon, abrir detalhes do pokemon e abrir a modal de attacks

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,7 +2,23 @@
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+      cy.visit('')
+      cy.contains('h1', 'Pokémon Cards')
+  })
+
+  it('Search Pikachu', () => {
+      cy.get('input').type('Pikachu')
+      cy.contains('.card', 'Pikachu')
+  })
+
+  it('Visits details page', () => {
+      cy.get('.card:first').click()
+      cy.url().should('include', '/details')
+      cy.contains('button', 'Back to list')
+  })
+
+  it('Open attack modal', () => {
+      cy.get('.list-item.clickable:first').click()
+      cy.get('.vm--modal').should('exist')
   })
 })
